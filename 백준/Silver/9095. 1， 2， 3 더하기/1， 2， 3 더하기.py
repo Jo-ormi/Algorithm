5d528c04e05b94ac03219eb5e86346b6fb9a01de
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-05-18 00:14:58 +0900

    Update 1， 2， 3 더하기.py
    파일이 겹쳐서 수정

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,25 +1,22 @@
- # 백준 9095번
 import sys
 
-def calList(n):
+def calList(list_len):
 
-  if n == 1:
+  if list_len == 1:
     return 1
-  elif n == 2:
+  elif list_len == 2:
     return 2
-  elif n == 3:
+  elif list_len == 3:
     return 4
   else:
-    return calList(n-1) + calList(n-2) + calList(n-3)
+    return calList(list_len-1) + calList(list_len-2) + calList(list_len-3)
 
 
 
 N = int(sys.stdin.readline())
 for i in range(N):
+    basic_list = [1] * int(sys.stdin.readline())
     cnt = 0
-    cnt += calList(int(sys.stdin.readline()))
+    
+    cnt += calList(len(basic_list))
     print(cnt, end=' ')
-  
-
-
-  
