efd2ba7c19d3e501d67f6bd28e79cfbda1fc7df5
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-05-18 00:11:40 +0900

    Create 1 , 2 , 3 더하기2.py
    메모리, 시간 동일

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1 , 2 , 3 더하기2.py
@@ -0,0 +1,21 @@
+# 백준 9095번
+import sys
+
+def calList(n):
+
+  if n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  else:
+    return calList(n-1) + calList(n-2) + calList(n-3)
+
+
+
+N = int(sys.stdin.readline())
+for i in range(N):
+    cnt = 0
+    cnt += calList(int(sys.stdin.readline()))
+    print(cnt, end=' ')
