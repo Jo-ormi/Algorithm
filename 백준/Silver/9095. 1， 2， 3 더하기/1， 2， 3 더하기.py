7a7b0185cf998213cf192ec4874e46713d1ee186
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-05-17 23:37:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,27 @@
+ # 백준 9095번
+import sys
+
+def calList(list_len):
+
+  if list_len == 1:
+    return 1
+  elif list_len == 2:
+    return 2
+  elif list_len == 3:
+    return 4
+  else:
+    return calList(list_len-1) + calList(list_len-2) + calList(list_len-3)
+
+
+
+N = int(sys.stdin.readline())
+for i in range(N):
+    basic_list = [1] * int(sys.stdin.readline())
+    cnt = 0
+    
+    cnt += calList(len(basic_list))
+    print(cnt, end=' ')
+  
+
+
+  
