9589fd5400c3e1c4fc1326a2ed64d864f3bb88d0
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-05-18 00:05:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,25 +1,23 @@
  # 백준 9095번
 import sys
 
-def calList(list_len):
+def calList(n):
 
-  if list_len == 1:
+  if n == 1:
     return 1
-  elif list_len == 2:
+  elif n == 2:
     return 2
-  elif list_len == 3:
+  elif n == 3:
     return 4
   else:
-    return calList(list_len-1) + calList(list_len-2) + calList(list_len-3)
+    return calList(n-1) + calList(n-2) + calList(n-3)
 
 
 
 N = int(sys.stdin.readline())
 for i in range(N):
-    basic_list = [1] * int(sys.stdin.readline())
     cnt = 0
-    
-    cnt += calList(len(basic_list))
+    cnt += calList(int(sys.stdin.readline()))
     print(cnt, end=' ')
   
 
