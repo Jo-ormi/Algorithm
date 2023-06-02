1803fc970d64357fbe87c44c472c5a6a87f6efce
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-06-02 17:35:22 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,15 +1,13 @@
 def solution(k, m, score):
     answer = 0
     
-    score.sort(reverse=True)
-    box = []
+    score.sort()
     i = 0
-    for i in range(0, len(score), i+m):   
-        if (i+m > len(score)):
-            break
-        box.append(score[i:i+m])
-    
-    for b in box:
-        answer += b[-1] * m 
+    cnt = 0
+    for i in range(len(score)-1, -1, -1):   
+        cnt += 1
+        if (m == cnt):
+            answer += score[i] * m 
+            cnt = 0
         
     return answer
