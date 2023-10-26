adb06ae776a388a32d50dbe88bf7c7cd2a68c0d1
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-10-26 14:41:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.84 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,25 @@
 from collections import deque
 def solution(maps):
-    answer = 0
-        
     n = len(maps)
     m = len(maps[0])
     
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
     
+    q = deque()
+    q.append((0,0))
     
-    queue = deque()
-    queue.append((0,0))
-    
-    while queue:
-        x, y = queue.popleft();
+    while q:
+        x, y = q.popleft()
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if nx < 0 or ny <0 or nx >= n or ny >= m or maps[nx][ny] != 1 :
-                continue
-                
-            if nx == 0 and ny == 0:
-                continue
-                
-            queue.append((nx,ny))
-            maps[nx][ny] += maps[x][y]
-            
         
-        
-    return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
+            if nx >= 0 and ny >= 0 and nx < n and ny < m and maps[nx][ny] == 1 :
+            
+                maps[nx][ny] += maps[x][y]
+                q.append((nx,ny))
+            
+    
+    return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1    
