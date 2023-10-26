64b4eb2b3899e49657871291991ebd9ed9aca5e2
Jo-ormi <81675744+Jo-ormi@users.noreply.github.com>
2023-10-26 14:19:23 +0900

    [level 2] Title: 타겟 넘버, Time: 1.45 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,16 @@
 
 def solution(numbers, target):
-    
     whole_sum = [0]
     
     answer = 0
-
+    
     for n in numbers:
         temp = []
         for w in whole_sum:
             temp.append(w+n)
             temp.append(w-n)
         whole_sum = temp
-    
+        
     for w in whole_sum:
         if w == target:
             answer += 1
